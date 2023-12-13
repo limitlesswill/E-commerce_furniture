8cad1b64711c2ea59a1a300a844164f1e1f2dc43
limitlesswill <limitlisswill@github.com>
2023-12-13 19:48:19 +0200

    - change names to copied ones

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,20 @@
+const btn = document.getElementsByClassName("ast-arrow-svg")[0];
+
+window.onload = () => {
+    window.document.body.onscroll = () => {
+
+        if (document.body.scrollTop || document.documentElement.scrollTop < 760) {
+            btn.style.display = "none";
+        }
+        else
+            btn.style.display = "block";
+    };
+
+    btn.addEventListener("click", () => {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+    });
+}
+
+
 
