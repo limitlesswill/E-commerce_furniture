6169e6c55c331801d4d7f05a0bba2f550bad96d5
Will <48535625+limitlesswill@users.noreply.github.com>
2023-12-15 04:39:32 +0200

    Create test.php
    back-end test

---
+++ api/test.php
@@ -0,0 +1,30 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET');
+
+$products = 
+  "{
+  "kitchen":[
+  {"id":"0","title":"title placeholder","thumbnail":"https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"},
+  {"id":"1","title":"title placeholder","thumbnail":"https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"}
+  ],
+  "diningtable":[
+  {"id":"0","title":"title placeholder","thumbnail":"https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"},
+  {"id":"1","title":"title placeholder","thumbnail":"https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"}
+  ],
+  "office":[
+  {"id":"0","title":"title placeholder","thumbnail":"https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"},
+  {"id":"1","title":"title placeholder","thumbnail":"https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"}
+  ],
+  "beds":[
+  {"id":"0","title":"title placeholder","thumbnail":"https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"},
+  {"id":"1","title":"title placeholder","thumbnail":"https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"}
+  ]
+  }";
+
+
+
+
+echo $products;
+
+?>
