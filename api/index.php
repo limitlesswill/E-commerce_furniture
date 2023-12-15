edec8f3390ca17a7bd07791bcfeb94eafd22bdf1
Will <48535625+limitlesswill@users.noreply.github.com>
2023-12-15 19:13:45 +0200

    json format
    as required should it be.

--- api/index.php
+++ api/index.php
@@ -3,24 +3,12 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET');
 
 $products = 
-  '{
-  "kitchen":[
-  {"id":"0","title":"title placeholder","thumbnail":"https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"},
-  {"id":"1","title":"title placeholder","thumbnail":"https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"}
-  ],
-  "diningtable":[
-  {"id":"0","title":"title placeholder","thumbnail":"https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"},
-  {"id":"1","title":"title placeholder","thumbnail":"https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"}
-  ],
-  "office":[
-  {"id":"0","title":"title placeholder","thumbnail":"https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"},
-  {"id":"1","title":"title placeholder","thumbnail":"https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"}
-  ],
-  "beds":[
-  {"id":"0","title":"title placeholder","thumbnail":"https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"},
-  {"id":"1","title":"title placeholder","thumbnail":"https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"}
-  ]
-  }';
+'[
+{"category":"kitchen","id":"0","name":"a table","thumbnail":"https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"},
+{"category":"diningtable","id":"1","name":"a table","thumbnail":"https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"},
+{"category":"office","id":"2","name":"a table","thumbnail":"https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"},
+{"category":"beds","id":"3","name":"a table","thumbnail":"https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"}
+]';
 
 
 
