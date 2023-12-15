2e3dcdb1fde6e77016358b2b40e6cc5b7fd5432a
Will <48535625+limitlesswill@users.noreply.github.com>
2023-12-15 15:21:48 +0200

    login style toggle

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,17 @@
 //for scrolling
 const btn = document.getElementsByClassName("ast-arrow-svg")[0];
+let login;
+let popup;
+let xit;
 
 window.onload = () => {
+ login = document.getElementById("login");
+ popup = document.getElementsByClassName("popup")[0];
+ xit = document.getElementById("xit");
+ login.addEventListener("click", togglePopup);
+ popup.addEventListener("click", togglePopup);
+ xit.addEventListener("click", togglePopup);
+    
     window.document.body.onscroll = () => {
 
         if (document.body.scrollTop || document.documentElement.scrollTop < 760) {
@@ -17,5 +27,19 @@ window.onload = () => {
     });
 }
 
+function togglePopup(e) {
+ if (e.target !== this) return;
 
+ let toggle = (popup.style.display == "") || (popup.style.display == "none");
 
+ if (toggle)
+  popup.style.display = "flex";
+ else
+  popup.style.display = "none";
+}
+
+function val(x) {
+ x.preventDefault();
+ console.log(x);
+ return false;
+}
